e53c305811c68b277e52b4cc9ff13e910e3a039a
AaviJit <avijit_barua@hotmail.com>
2018-09-03 17:02:20 +0600

    problem solved

---
+++ src/main/java/org/avijit/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package org.avijit.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "welcome";
+    }
+
+}
